d37c17ebadc42d1d81ea0058ae07b9f21c1fe1bc
jaysm12 <jaizm12@gmail.com>
2019-06-21 00:08:06 +0700

    template, belom refactor pake js file component

---
+++ javascript/main.js
@@ -0,0 +1,73 @@
+new Vue({
+  el: '#app',
+  data: {
+    page: '',
+    loginStatus: true,
+    error: '',
+    login: {
+      email: '',
+      password: ''
+    },
+    register: {
+      email: '',
+      password: ''
+    },
+    errorLogin: {
+      status: false,
+      msg: ''
+    },
+    errorRegister: {
+      status: false,
+      msg: ''
+    },
+    loggedUser: {
+      firstName: 'Michael',
+      lastName: 'Scott',
+      email: ''
+    },
+    successRegister: false
+  },
+  methods: {
+    registerUser() {
+      let input = this.registerUser
+      axios({ 
+        method: 'POST',
+        url: `http://localhost:3000/api/users/register`,
+        data: input
+      })
+      .then(({data}) => {
+        this.successRegister = true
+      })
+      .catch(next)
+    },
+    loginUser() {
+      let input = this.loginUser
+      axios({ 
+        method: 'POST',
+        url: `http://localhost:3000/api/users/register`,
+        data: input
+      })
+      .then(({data}) => {
+        localStorage.setItem('token', data.token)
+        this.page = 'article'
+        this.loginStatus = true
+      })
+      .catch(next)
+    },
+    addNewReso() {
+
+    },
+    
+    logoutUser() {
+      localStorage.clear()
+      this.loginStatus = false
+      this.page = 'login'
+      this.successRegister = true
+    }
+  },
+  computed: {
+    fullName() {
+      return this.loggedUser.firstName + ' ' + this.loggedUser.lastName
+    }
+  }
+})
