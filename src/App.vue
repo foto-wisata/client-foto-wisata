a0c3384d1c20be95d9bda56d053a344840d786f7
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-06-21 15:36:34 +0700

    error

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,132 @@
 <template>
-  <div id="navbar">
-    <div class="nav-content logo-nav" @click="page = 'article'">
-        <i class="far fa-calendar-check logonya"></i>
-      <div class="nav-content">Resolution Maker</div>
-    </div>
-      <div class="nav-content logout-nav" v-if="loginStatus === true" @click="logoutUser">
-      <div class="nav-content">Sign Out</div>
-      <i class="fas fa-sign-out-alt logonya"></i>
-    </div>
-    <div class="nav-content login-nav" v-if="!loginStatus && page != 'login'" @click="page = 'login'">
-      <div class="nav-content">Sign In</div>
-      <i class="fas fa-sign-in-alt logonya"></i>
-    </div>
-  </div>  
+  <div>
+    <navbar :loginStatus="loginStatus" :page="page" />
+    <!-- <p style="text-align: center" id="successAlert" v-show="successRegister === true" class="primary-container">Success created an account {{register.email}} !</p> -->
+    <login-page></login-page>
+      <!-- <div v-else>
+        <main-content />
+      </div> -->
+  </div>
 </template>
+
 <script>
+import list from "./components/list";
+import LoginPage from "./components/LoginPage.vue";
+import maincontent from './components/mainContent';
 import modal from "./components/modal";
-import login from "./components/login"
+import navbar from './components/navbar';
+
+const BASE_URL = `http://localhost:3000/`;
 
 export default {
-  data() {
-    return {
-      page: 'login',
-      loginStatus: localStorage.getItem("token") ? true : false,
-      error: '',
-      login: {
-        email: '',
-        password: ''
-      },
-      register: {
-        email: '',
-        password: '',
-        username : ''
-      },
-      errorLogin: {
-        status: false,
-        msg: ''
-      },
-      errorRegister: {
-        status: false,
-        msg: ''
-      },
-      loggedUser: {
-        firstName : localStorage.getItem("payload.email")
-      },
-      successRegister: false,
-      showModal: false
+  components: {
+    'navbar': navbar,
+    LoginPage,
+    'main-content': maincontent,
+  },
+  data: {
+    page: "login",
+    loginStatus: false,
+    error: "",
+    resolution: {
+      description: null,
+      image: null,
+    },
+    login: {
+      email: "",
+      password: ""
+    },
+    register: {
+      email: "",
+      password: ""
+    },
+    errorLogin: {
+      status: false,
+      msg: ""
+    },
+    errorRegister: {
+      status: false,
+      msg: ""
+    },
+    errorResolution: {
+      status: false,
+      msg: ""
+    },
+    loggedUser: {
+      firstName: localStorage.getItem("resolusiemail")
+    },
+    resolution: [],
+    successRegister: false,
+    showModal: false
+  },
+  created() {
+    let token = window.localStorage.getItem("resolusitoken");
+    this.loggedUser.fi = window.localStorage.getItem("resolusiemail");
+    if (token) {
+      this.loginStatus = true;
+    }
+  },
+  methods: {
+    axiosConfig() {
+      return {
+        headers: {
+          token: window.localStorage.getItem("resolusitoken")
+        }
+      };
+    },
+    a_login() {
+      axios
+        .post(`${BASE_URL}/user/signin`, this.login, this.axiosConfig())
+        .then(res => {
+          window.localStorage.setItem("resolusitoken", res.data.token);
+          window.localStorage.setItem("resolusiemail", this.login.email);
+          this.loginStatus = true;
+        })
+        .catch(err => {
+          this.errorLogin.status = true;
+          this.errorLogin.msg = err.message;
+        });
+    },
+    a_register() {
+      axios
+        .post(`${BASE_URL}/user/signup`, this.register, this.axiosConfig())
+        .then(res => {
+          window.localStorage.setItem("resolusitoken", res.data.token);
+          window.localStorage.setItem("resolusiemail", this.login.email);
+        })
+        .catch(err => {
+          this.errorRegister.status = true;
+          this.errorRegister.msg = err.message;
+        });
+    },
+    imageFileChange(e) {
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length) return;
+      this.resolution.image = files[0]
+    },
+    postResolution() {  
+      axios
+        .post(`${BASE_URL}/resolutions/`, this.resolution, this.axiosConfig())
+        .then(() => {
+
+        })
+        .catch(err => {
+          this.errorResolution.status = true
+          this.errorResolution.msg = err.message
+        })
+    }, 
+    fetchResolution() {
+      axios
+        .get(`${BASE_URL}/resolutions/`, this.axiosConfig())
+        .then(res => {
+          this.resolution = res.data
+          this.errorResolution.status = false
+        })
+        .catch(err => {
+          this.errorResolution.status = true
+          this.errorResolution.msg = err.message
+        })
     }
   }
-}
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,83 +1,84 @@
-import App from './App.vue'
 import Vue from "vue"
+import App from './App.vue'
 import 'materialize-css/dist/css/materialize.css'
 
 new Vue(App).$mount('#app')
 
 
 
-new Vue({
-  el: '#app',
-  data: {
-    page: 'login',
-    loginStatus: localStorage.getItem("token") ? true : false,
-    error: '',
-    login: {
-      email: '',
-      password: ''
-    },
-    register: {
-      email: '',
-      password: '',
-      username : ''
-    },
-    errorLogin: {
-      status: false,
-      msg: ''
-    },
-    errorRegister: {
-      status: false,
-      msg: ''
-    },
-    loggedUser: {
-      firstName : localStorage.getItem("payload.email")
-    },
-    successRegister: false,
-    showModal: false
-  },
-  methods: {
-    registerUser() {
-      let input = this.register
-      axios({ 
-        method: 'POST',
-        url: `http://localhost:3000/user/signup`,
-        data: input
-      })
-      .then(({data}) => {
-        this.successRegister = true
-      })
-      .catch(next)
-    },
-    loginUser() {
-      let input = this.login
-      axios({ 
-        method: 'POST',
-        url: `http://localhost:3000/user/signin`,
-        data: input
-      })
-      .then(({data}) => {
-        console.log(input, 'ini input')
-        localStorage.setItem('token', data.token)
-        localStorage.setItem('payload', data.payload)
-        this.page = 'article'
-        this.loginStatus = true
-      })
-      .catch(next)
-    },
-    addNewReso() {
 
-    },
+// new Vue({
+//   el: '#app',
+//   data: {
+//     page: 'login',
+//     loginStatus: localStorage.getItem("token") ? true : false,
+//     error: '',
+//     login: {
+//       email: '',
+//       password: ''
+//     },
+//     register: {
+//       email: '',
+//       password: '',
+//       username : ''
+//     },
+//     errorLogin: {
+//       status: false,
+//       msg: ''
+//     },
+//     errorRegister: {
+//       status: false,
+//       msg: ''
+//     },
+//     loggedUser: {
+//       firstName : localStorage.getItem("payload.email")
+//     },
+//     successRegister: false,
+//     showModal: false
+//   },
+//   methods: {
+//     registerUser() {
+//       let input = this.register
+//       axios({ 
+//         method: 'POST',
+//         url: `http://localhost:3000/user/signup`,
+//         data: input
+//       })
+//       .then(({data}) => {
+//         this.successRegister = true
+//       })
+//       .catch(next)
+//     },
+//     loginUser() {
+//       let input = this.login
+//       axios({ 
+//         method: 'POST',
+//         url: `http://localhost:3000/user/signin`,
+//         data: input
+//       })
+//       .then(({data}) => {
+//         console.log(input, 'ini input')
+//         localStorage.setItem('token', data.token)
+//         localStorage.setItem('payload', data.payload)
+//         this.page = 'article'
+//         this.loginStatus = true
+//       })
+//       .catch(next)
+//     },
+//     addNewReso() {
 
-    logoutUser() {
-      localStorage.clear()
-      this.loginStatus = false
-      this.page = 'login'
-      this.successRegister = true
-    }
-  },
-  computed: {
-    fullName() {
-      return this.loggedUser.firstName + ' ' + this.loggedUser.lastName
-    }
-  }
-})
+//     },
+
+//     logoutUser() {
+//       localStorage.clear()
+//       this.loginStatus = false
+//       this.page = 'login'
+//       this.successRegister = true
+//     }
+//   },
+//   computed: {
+//     fullName() {
+//       return this.loggedUser.firstName + ' ' + this.loggedUser.lastName
+//     }
+//   }
+// })
